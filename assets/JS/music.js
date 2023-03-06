e892da88412f79acc984fd4489c71ed15986a704
Sabuhi Gasimzada <96132884+s3bu7i@users.noreply.github.com>
2023-03-07 01:53:45 +0400

    0

--- assets/JS/music.js
+++ assets/JS/music.js
@@ -12,6 +12,8 @@ class Music {
 const musicList = [
     new Music("Chain","Fleetwood","1.jpeg","1.mp3"),
     new Music("Dance Me To The End Of Love","Leonard Cohen","2.jpeg","2.mp3"),
-    new Music("We Own It","Wiz Khalifa","3.jpeg","3.mp3")
+    new Music("We Own It","Wiz Khalifa","3.jpeg","3.mp3"),
+    new Music("TV Girl","Lovers Rock","4.jpeg","4.mp3"),
+    new Music("Dönməlisən","Neysan","5.jpeg","5.mp3")
 
 ];
